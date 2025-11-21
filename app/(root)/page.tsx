fb74c098e600f0e25046eb0ec9307a7869e82571
akshat_1074 <valaniakshat1074@gmail.com>
2025-11-21 22:35:59 +0530

    Added trading chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-screen">page</div>
+    <div className="flex min-h-screen home-wrapper">
+      <section className="grid w-full gap-8 home-section">
+         <div className="md:col-span-1 xl:col-span-1">
+            <TradingViewWidget>
+              
+            </TradingViewWidget>
+         </div>
+      </section>
+
+    </div>
   )
 }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image"
+import Link from "next/link"
+import Navitems from "./Navitems"
+import UserDropdown from "./UserDropdown"
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image src="/assests/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer"/>
+        </Link>
+        <nav className="hidden sm:block">
+          <Navitems/>
+        </nav>
+        <UserDropdown/>
+
+      </div>
+   </header>
   )
 }
 
