89a5a8586900106179a1d53e192914cc408bd14b
akshat_1074 <valaniakshat1074@gmail.com>
2025-11-28 22:56:42 +0530

    streak

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+
+const SignIN = () => {
+  return (
+    <div>SignIN</div>
+  )
+}
+
+export default SignIN
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <>
+     <h1 className='form-title'>Sign Up </h1>
+    </>
+  )
+}
+
+export default SignUp
