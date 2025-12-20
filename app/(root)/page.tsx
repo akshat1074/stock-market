95537615d8c4a84dff5219fee29bccfbf0cff27c
akshat_1074 <valaniakshat1074@gmail.com>
2025-12-20 23:14:45 +0530

    final

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,18 @@
-import TradingViewWidget from "@/components/TradingViewWidgets";
+import TradingViewWidget from "@/components/TradingViewWidget";
 import {
     HEATMAP_WIDGET_CONFIG,
     MARKET_DATA_WIDGET_CONFIG,
     MARKET_OVERVIEW_WIDGET_CONFIG,
     TOP_STORIES_WIDGET_CONFIG
 } from "@/lib/constants";
-
+import {sendDailyNewsSummary} from "@/lib/inngest/functions";
 
 const Home = () => {
-  const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+    const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
 
-
-  return (
-    <div className="flex min-h-screen home-wrapper">
-      <section className="grid w-full gap-8 home-section">
+    return (
+        <div className="flex min-h-screen home-wrapper">
+          <section className="grid w-full gap-8 home-section">
               <div className="md:col-span-1 xl:col-span-1">
                   <TradingViewWidget
                     title="Market Overview"
@@ -48,9 +47,8 @@ const Home = () => {
                     />
                 </div>
             </section>
-
-    </div>
-  )
+        </div>
+    )
 }
 
-export default Home
+export default Home;
